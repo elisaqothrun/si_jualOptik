81c0072d46e110e94cdb9484ef8cb63aba3de339
elisaqothrun <56098639+elisaqothrun@users.noreply.github.com>
2019-11-27 23:13:37 +0700

    hapuskeranjang

---
+++ hapuskeranjang.php
@@ -0,0 +1,27 @@
+<<<<<<< HEAD
+<<<<<<< HEAD
+<?php
+session_start();
+$ID_BARANG = $_GET["id"];
+unset($_SESSION["keranjang"][$ID_BARANG]);
+echo "<script>alert('barang dihapus dari keranjang');</script>";
+echo "<script>location='keranjang.php';</script>";
+=======
+<?php
+session_start();
+$ID_BARANG = $_GET["id"];
+unset($_SESSION["keranjang"][$ID_BARANG]);
+
+echo "<script>alert('Barang dihapus dari keranjang');</script>";
+echo "<script>location='keranjang.php';</script>";
+>>>>>>> 63b846c99e11f17da3bff0a80f9f392a6b44c586
+=======
+<?php
+session_start();
+$ID_BARANG = $_GET["id"];
+unset($_SESSION["keranjang"][$ID_BARANG]);
+
+echo "<script>alert('Barang dihapus dari keranjang');</script>";
+echo "<script>location='keranjang.php';</script>";
+>>>>>>> 63b846c99e11f17da3bff0a80f9f392a6b44c586
+?>
