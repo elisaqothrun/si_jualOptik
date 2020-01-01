1ac6db2f6e94954d70548dcfa0d46d406e6ef9f0
ajengayupangesti <56049521+ajengayupangesti@users.noreply.github.com>
2020-01-01 23:14:31 +0700

    Navbar
    Navbar

--- menu.php
+++ menu.php
@@ -1,15 +1,45 @@
 <!-- Navbar -->
-	<nav class="navbar navbar-default">
-		<div class="container">
+<!DOCTYPE html>
+<html>
+<head>
+  <title></title>
+</head>
+<body>
 
-			<ul class="nav navbar-nav">
-				
-					<li><a href="index.php">Optik Surya</li>
-					<li><a href="kategori.php">Kategori</li>
-					<li><a href="keranjang.php">Keranjang</li>
-				  	<li><a href="daftar.php">Daftar</li>
-					<li><a href="login_pembeli.php">Login</li>
-				
-			</ul>
-		</div>
-	</nav>
+</body>
+</html>
+     <link rel="stylesheet" href="assets/css/bootstrap.css">
+     <link rel="stylesheet" type="text/css" href="assets/css/font-awesome.css">
+     <link rel="stylesheet" type="text/css" href="style_footer.css">
+     <nav class="navbar navbar-expand-lg navbar-light bg-custom fixed-top" id="mainNav" style="border-radius: 0;">
+      <div class="container">
+      <a href="index.php" class="navbar-brand"><img src="assets/img/logo-brand.png" height="20"></a>
+      <ul class="nav navbar-nav">
+          <li><a href="kategori.php">Kategori</a></li>
+          <li><a href="keranjang.php">Keranjang</a></li>
+          <li><a href="riwayat.php">Riwayat Belanja</a></li>
+          <li><a href="daftar.php">Daftar</a></li>
+
+            <!--jk sudah login(ada session pembeli)-->
+            <?php if (isset($_SESSION["pembeli"])): ?> 
+            <li><a href="logout.php">Logout</a></li>
+             <!--jika belum login belum ada session pembeli--> 
+            <?php else : ?>
+              <li><a href="login.php">Login</a></li>
+            <?php endif ?>
+
+      </ul>
+
+      <form action="pencarian.php" method="get" class="navbar-form navbar-right">
+        <input type="text" class="form-control" placeholder="masukan nama barang" name="keyword">
+        <button class="btn btn-primary"><i class="fa fa-search"></i> Cari</button>
+      </form>
+     
+    <!--<form action="datacoba.php" method="post" class="navbar-form navbar-right">
+            <input type="text" name="s_keyword" id="s_keyword" class="form-control">
+            <button id="search" name="search" class="btn btn-primary"><i class="fa fa-search"></i> Cari</button>     
+    </form>-->
+ 
+<div class="data"></div>
+    </div>
+  </nav>
